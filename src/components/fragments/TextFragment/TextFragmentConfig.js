baf2466595e0321504949b581c1c12cdbf26f4a2
Stuart Lynn <stuart.lynn@gmail.com>
2018-10-07 16:10:36 -0400

    updating text fragment config

--- src/components/fragments/TextFragment/TextFragmentConfig.js
+++ src/components/fragments/TextFragment/TextFragmentConfig.js
@@ -6,7 +6,7 @@ export const FragmentVersion = '1.0'
 
 export const TextFragmentPropTypes = {
   ...CommonFragmentPropTypes,
-  propmpt: PropTypes.string.isRequired,
+  prompt: PropTypes.string.isRequired,
   icon: PropTypes.string,
   children: PropTypes.node,
   className: PropTypes.string,
